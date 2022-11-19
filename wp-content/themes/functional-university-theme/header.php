036a52c0b0e16571f663c12ef784768557a7d4c6
podyankee <podyankee@gmail.com>
2022-11-19 06:34:20 +0300

    test commit

--- wp-content/themes/functional-university-theme/header.php
+++ wp-content/themes/functional-university-theme/header.php
@@ -12,7 +12,7 @@
 	<header class="site-header">
 		<div class="container">
 			<h1 class="school-logo-text float-left">
-				<a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a>
+				<a href="<?php echo site_url(); ?>"><strong>Test</strong> University</a>
 			</h1>
 			<a href="<?php echo esc_url(site_url('/search')); ?>" class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
 			<i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
