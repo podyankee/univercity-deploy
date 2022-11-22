8f7e44154b6c2955e46aa52bc00b55c5ac2c9b92
podyankee <podyankee@gmail.com>
2022-11-22 15:04:19 +0300

    block types attributes

---
+++ wp-content/plugins/are-you-paying-attention/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-element'), 'version' => 'da28e6f6408c485bbf30');
---
+++ wp-content/plugins/are-you-paying-attention/index.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+
+Plugin Name: Are You Paying Attention Quiz
+Description: Give your readers a multiple choice question.
+Version: 1.0
+Author: Kostyantyn
+Author URI: https://facebook.com
+
+*/
+
+if(! defined( 'ABSPATH' )) exit; // Exit if accessed directly
+
+class AreYouPayingAttention {
+	function __construct () {
+		add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+	}
+
+	function adminAssets () {
+		wp_enqueue_script( 'ournewblocktype', plugin_dir_url(  __FILE__ ) . 'build/index.js', array('wp-blocks', 'wp-element'));
+	}
+}
+$areYouPayingAttention = new  AreYouPayingAttention();
